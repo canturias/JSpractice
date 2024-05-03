c7807b194f997f0384579b3919fc43462c9d324a
Chris Can't <christiangcanturias@gmail.com>
2024-05-03 19:20:51 +0800

    Small fixes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ document.getElementById("areaButton").onclick = function() {
     width = parseFloat(document.getElementById("width").value);
     let area = length * width;
     if (isNaN(length) || isNaN(width)) {
-        document.getElementById("area").textContent = `Area Calculator — Invalid`;
+        document.getElementById("area").textContent = `Area Calculator — Error`;
     }
     else {
         document.getElementById("area").textContent = `Area Calculator — ${area.toFixed(2)}`;
@@ -34,7 +34,6 @@ document.getElementById("areaButton").onclick = function() {
 let tempInput, tempResult;
 
 checkSelected();
-
 document.getElementById("ctf").addEventListener("click", checkSelected);
 document.getElementById("ftc").addEventListener("click", checkSelected);
 document.getElementById("tempInput").addEventListener("change", checkSelected);
@@ -44,24 +43,34 @@ function checkSelected() {
     var ftcRadio = document.getElementById("ftc");
 
     tempInput = parseFloat(document.getElementById("tempInput").value);
+
     console.log(tempInput);
 
     if (ctfRadio.checked) {
-        console.log("CTF");
         document.getElementById("tempTitle").textContent = "Celsius to Fahrenheit";
         document.getElementById("tempLabel").textContent = "Celsius";
+        document.getElementById("tempInput").placeholder = "\u00B0C";
         document.getElementById("tempButton").onclick = function() {
             tempResult = (tempInput * 9 / 5) + 32;
-            document.getElementById("tempTitle").textContent = `Celsius to Fahrenheit — ${tempResult.toFixed(2)}\u00B0F`;
-        }
-            
+            if (isNaN(tempInput)) {
+                document.getElementById("tempTitle").textContent = `Celsius to Fahrenheit — Error`;
+            }
+            else {
+                document.getElementById("tempTitle").textContent = `Celsius to Fahrenheit — ${tempResult.toFixed(2)}\u00B0F`;
+            }
+        }      
     } else if (ftcRadio.checked) {
-        console.log("FTC");
         document.getElementById("tempTitle").textContent = "Fahrenheit to Celsius";
         document.getElementById("tempLabel").textContent = "Fahrenheit";
+        document.getElementById("tempInput").placeholder = "\u00B0F";
         document.getElementById("tempButton").onclick = function() {
             tempResult = (tempInput - 32) * 5 / 9;
-            document.getElementById("tempTitle").textContent = `Fahrenheit to Celsius — ${tempResult.toFixed(2)}\u00B0C`;
+            if (isNaN(tempInput)) {
+                document.getElementById("tempTitle").textContent = `Celsius to Fahrenheit — Error`;
+            }
+            else {
+                document.getElementById("tempTitle").textContent = `Fahrenheit to Celsius — ${tempResult.toFixed(2)}\u00B0C`;
+            } 
         }
     }
 }
